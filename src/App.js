d94496413a658f44f2082169a79dc8fd46a12a1e
Taimoor khan <khant8k83@gmail.com>
2020-12-09 18:56:55 +0500

    feat: Controlled form input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [state, setState] = useState({
+    age: "",
+    gender: "",
+    weight: "",
+    heightFeet: "",
+    heightInches: "",
+    activity: "",
+    bmr: "",
+  });
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    // ha
+    switch (name) {
+      case "age":
+        return setState({ ...state, age: value });
+      case "gender":
+        return setState({ ...state, gender: value });
+      case "weight":
+        return setState({ ...state, weight: value });
+      case "heightFeet":
+        return setState({ ...state, heightFeet: value });
+      case "heightInches":
+        return setState({ ...state, heightInches: value });
+      case "activity":
+        return setState({ ...state, activity: value });
+      default:
+        return console.log("something went wrong");
+    }
+  };
   return (
     <div id="bmrcalc">
       <div className="form">
@@ -8,11 +40,25 @@ function App() {
         <div className="inputwrap">
           <label className="label">Gender</label>
           <label>
-            <input type="radio" className="genderF" name="gender" value="1" />
+            <input
+              type="radio"
+              className="genderF"
+              name="gender"
+              value="1"
+              onChange={handleChange}
+              checked={state.gender === "1"}
+            />
             Female
           </label>
           <label>
-            <input type="radio" className="genderM" name="gender" value="2" />
+            <input
+              type="radio"
+              className="genderM"
+              name="gender"
+              value="2"
+              onChange={handleChange}
+              checked={state.gender === "2"}
+            />
             Male
           </label>
         </div>
@@ -24,6 +70,8 @@ function App() {
             className="weight"
             min="0"
             max="999"
+            onChange={handleChange}
+            value={state.weight}
           />
         </div>
         <div className="inputwrap">
@@ -34,6 +82,8 @@ function App() {
             className="heightFeet"
             min="0"
             max="8"
+            onChange={handleChange}
+            value={state.heightFeet}
           />
           <input
             type="number"
@@ -41,17 +91,32 @@ function App() {
             className="heightInches"
             min="0"
             max="11"
+            onChange={handleChange}
+            value={state.heightInches}
           />
         </div>
         <div className="inputwrap">
           <label className="label">Age in years</label>
-          <input type="number" className="age" name="age" min="0" max="120" />
+          <input
+            type="number"
+            className="age"
+            name="age"
+            min="0"
+            max="120"
+            onChange={handleChange}
+            value={state.age}
+          />
         </div>
         <button type="button">Calculate BMR</button>
         <div className="workout">
           <div className="inputwrap">
             <label className="label">Workout in a Week</label>
-            <select className="activity" name="activity">
+            <select
+              className="activity"
+              name="activity"
+              onChange={handleChange}
+              value={state.activity}
+            >
               <option value="">Select your Activity</option>
               <option value="1.2">
                 Sedentary (Very little or no exercise, and desk job)
